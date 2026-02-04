e0cbb0f9967e21d89fa147f663fe854e64ef2404
Khaled Esmail <kesmail@pdx.edu>
2026-02-04 04:45:13 -0800

    Restrict CORS origin

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 #initialize the flask app
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://50.19.182.206:5173"])
     from .routes import routes
     app.register_blueprint(routes)
    
